6bd373d0f4759c341c45a92b75a94706d4fa3546
Osman Hassan <ama@Amas-MacBook-Pro.local>
2018-10-27 23:53:42 -0500

    files

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -3,11 +3,11 @@ $(function () {
     console.log("page loaded");
 })
 
-var searchArray = ['Dunk', 'alleyoop', 'freethrow'];
+var searchArray = ['dunk', 'alleyoop', 'freethrow'];
 
-function populateButtons(searchArray, classToAdd, areaToAddTo) {
+function populateButtons(searchArray,classToAdd,areaToAddTo) {
     $(areaToAddTo).empty();
-    for (var i = 0; i < searchArray.length; i++) {
+    for (var i=0; i<searchArray.length;i++) {
         var a = $('<button>');
         a.addClass(classToAdd);
         a.attr('data-type', searchArray[i]);
@@ -20,18 +20,45 @@ function populateButtons(searchArray, classToAdd, areaToAddTo) {
 
 }
 
-$(document).on('click','.searchButton',function(){
-    var move= $(this).data('move');
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?q'+move+'&api_key=LpVrOSwi9J91ZiRnd5KVdE56clEx49Cx&limit=10';
-    $.ajax({url:queryURL,method:'GET'})
-        .done(function(response){
-           for (var i=0;i<response.data.length;i++){
-                var searchDiv
-
-           }
-
+$(document).on('click', '.searchButton', function() {
+    $('#searches').empty();
+    var move = $(this).data('searchArray[i]');
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?q=' + move + '&api_key=LpVrOSwi9J91ZiRnd5KVdE56clEx49Cx&limit=10';
+    $.ajax({ url: queryURL, method: 'GET' })
+        .done(function(response) {
+            for (var i = 0; i<response.data.length; i++) {
+                var searchDiv = $('<div class="search-item">');
+                var rating = response.data[i].rating;
+                var p = $('<p>').text('Rating: ' + rating);
+                var animated = response.data[i].images.fixed_height.url;
+                var still = response.data[i].images.fixed_height_still.url;
+                var image = $('<img>');
+                image.attr('src', still);
+                image.attr('data-still', still);
+                image.attr('data-animated', animated);
+                image.attr('data-state', 'still');
+                image.addClass('searchImage');
+                searchDiv.append(p);
+                searchDiv.append(image);
+                $('#searches').append(searchDiv);
+            }
 
         })
 
+    })
 
-})
+    $('#addSearch').on('click',function(){
+        var newSearch = $('input').eq(0).val();
+        searchArray.push(newSearch);
+        populateButtons(searchArray,'searchButton','#topButtons');
+        return false;
+    })
+    
+
+
+
+        
+
+
+    
+
